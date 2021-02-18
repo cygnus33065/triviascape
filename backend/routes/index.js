92ec9977d4f0555fd521ca7e780c4d76b68e924f
cygnus33065 <cygnus_33065@yahoo.com>
2021-02-18 12:30:52 -0500

    first api route created and tested

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,8 @@
 const express = require ('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 router.get('/hello/world', function(req,res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
@@ -7,5 +10,3 @@ router.get('/hello/world', function(req,res) {
 });
 
 module.exports = router;
-
-
