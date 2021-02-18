b9d9cd60bda25ff0968653cf6ebbb407de60f6cd
cygnus33065 <cygnus_33065@yahoo.com>
2021-02-18 17:36:47 -0500

    backend auth complete

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -2,8 +2,7 @@ const { validationResult } = require('express-validator')
 
 const handleValidationErrors = (req, _res, next) => {
   const validationErrors = validationResult(req);
-
-  if(validationErrors.isEmpty()) {
+  if(!validationErrors.isEmpty()) {
     const errors = validationErrors
       .array()
       .map((error) => `${error.msg}`);
