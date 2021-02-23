35a9ed2a1e0580d4542903b7008d9ed81b6bba9c
cygnus33065 <cygnus_33065@yahoo.com>
2021-02-23 13:29:03 -0500

    create deck backend route complete and working

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const sessionRouter = require('./session.js')
 const usersRouter = require('./users.js')
+const decksRouter = require('./decks')
 
 const router = express.Router();
 
 router.use('/session', sessionRouter)
-
 router.use('/users', usersRouter)
+router.use('/decks', decksRouter)
 
 
 
